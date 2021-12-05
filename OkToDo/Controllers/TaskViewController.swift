ced97beb2f12d6d1d5c32f0b62fd8c7f5fa74c50
Oleksandr Kurtsev <kurtsev0103@gmail.com>
2021-12-05 13:44:31 +0100

    Changed TaskViewController to TableViewController

--- OkToDo/Controllers/TaskViewController.swift
+++ OkToDo/Controllers/TaskViewController.swift
@@ -8,18 +8,40 @@
 import UIKit
 import CoreData
 
-class TaskViewController: UIViewController {
+class TaskViewController: UITableViewController {
     
     private let context: NSManagedObjectContext
+    private let identifier = String(describing: TaskTableViewCell.self)
     
-    private let tableView = UITableView()
-    private let datePicker = UIDatePicker()
+    private lazy var nameTextField: CustomTextField = {
+        let nameTextField = CustomTextField()
+        nameTextField.placeholder = kPlaceholderName
+        nameTextField.delegate = self
+        return nameTextField
+    }()
     
-    private let nameTextField = CustomTextField()
-    private let categoryTextField = CustomTextField()
+    private lazy var categoryLabel: CustomLabel = {
+        let categoryLabel = CustomLabel(kCategoryEmptyString)
+        categoryLabel.textColor = Colors.placeholder
+        return categoryLabel
+    }()
     
-    private let saveButton = CustomButton(type: .saveButton)
-    private let cancelButton = CustomButton(type: .cancelButton)
+    private lazy var datePickerView: DatePickerView = {
+        let datePickerView = DatePickerView()
+        return datePickerView
+    }()
+    
+    private lazy var saveButton: CustomButton = {
+        let saveButton = CustomButton(type: .saveButton)
+        saveButton.addTarget(self, action: #selector(saveAction), for: .touchUpInside)
+        return saveButton
+    }()
+    
+    private lazy var cancelButton: CustomButton = {
+        let cancelButton = CustomButton(type: .cancelButton)
+        cancelButton.addTarget(self, action: #selector(cancelAction), for: .touchUpInside)
+        return cancelButton
+    }()
     
     init(context: NSManagedObjectContext) {
         self.context = context
@@ -32,54 +54,85 @@ class TaskViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupMainParams()
-        setupConstraints()
-        setupDatePicker()
-        setupTextFields()
-        setupButtons()
-    }
-    
-    private func setupMainParams() {
-        navigationItem.title = kNewTaskString
-        navigationItem.hidesBackButton = true
-        view.backgroundColor = Colors.mainWhite
-    }
-    
-    private func setupDatePicker() {
-        datePicker.datePickerMode = .date
-        datePicker.locale = .current
-//        datePicker.preferredDatePickerStyle = .compact
-        datePicker.tintColor = Colors.niceBlue
         
-        let calendar = Calendar(identifier: .gregorian)
-        let components = DateComponents()
-        datePicker.minimumDate = calendar.date(byAdding: components, to: Date())
-    }
-    
-    private func setupTextFields() {
-        nameTextField.delegate = self
-        categoryTextField.delegate = self
-        nameTextField.placeholder = kPlaceholderName
-        categoryTextField.placeholder = kPlaceholderCategory
-    }
-    
-    private func setupButtons() {
-        cancelButton.addTarget(self, action: #selector(cancelAction), for: .touchUpInside)
-        saveButton.addTarget(self, action: #selector(saveAction), for: .touchUpInside)
+        tableView.register(TaskTableViewCell.self, forCellReuseIdentifier: identifier)
+        tableView.separatorColor = .clear
+        
+        navigationItem.hidesBackButton = true
+        navigationItem.title = kNewTaskString
+        view.backgroundColor = Colors.mainWhite
     }
     
     // MARK: - Actions
     
     @objc private func saveAction() {
+        let (isVerified, nameString) = Utils.validateStringLength(nameTextField.text)
+        
+        guard isVerified else {
+            showAlert(title: kAlertError, message: kAlertStringLengthError)
+            return
+        }
+        
         let task = Task(context: context)
-        task.name = nameTextField.text
-        task.date = datePicker.date
+        task.name = nameString
+        task.date = datePickerView.date
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-        cancelAction()
+        
+        guard let navigationController = navigationController else { return }
+        navigationController.popViewControllerWithCompletion {
+            navigationController.showAlert(title: kAlertTitleCongr, message: kAlertNewTaskMessage)
+        }
     }
     
     @objc private func cancelAction() {
+        guard nameTextField.text?.isEmpty ?? true else {
+            showAlert(title: nil, message: kConfirmCancelMessage) { [unowned self] in
+                navigationController?.popViewController(animated: true)
+            }
+            return
+        }
+        
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource && UITableViewDelegate
+
+extension TaskViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? TaskTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        switch indexPath.row {
+        case 0: cell.configure(view: nameTextField)
+        case 1: cell.configure(view: categoryLabel)
+        case 2: cell.configure(view: datePickerView)
+        case 3: cell.configure(view: cancelButton)
+        case 4: cell.configure(view: saveButton)
+        default: break
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row == 1 {
+            let vc = CategoryViewController(context: context)
+            vc.categoryDelegate = self
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
 
